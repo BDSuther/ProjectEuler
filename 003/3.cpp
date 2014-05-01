c95c05d8c4efc838d33933d14e021c8248873ea9
BDSuther <CHV.Brian@gmail.com>
2014-05-01 02:19:42 -0400

    fix all on one line

--- 003/3.cpp
+++ 003/3.cpp
@@ -1,1 +1,32 @@
-#include <iostream>?#include <vector>?#include <stdint.h>??/*The prime factors of 13195 are 5, 7, 13 and 29.? ? What is the largest prime factor of the number 600851475143 ?? */??using namespace std;??int largestPrime (uint64_t num){??	int divisor=2;??	while (divisor != num){?		if ((num % divisor)==0) {?			num = num/divisor;?		} else {?			divisor++;?		}?	}??	return divisor;??}?int main(){??	cout << "The largest prime factor of 600851475143 is " << largestPrime(600851475143) << "." << endl;??}??
+#include <iostream>
+#include <vector>
+#include <stdint.h>
+
+/*The prime factors of 13195 are 5, 7, 13 and 29.
+ 
+ What is the largest prime factor of the number 600851475143 ?
+ */
+
+using namespace std;
+
+int largestPrime (uint64_t num){
+
+	int divisor=2;
+
+	while (divisor != num){
+		if ((num % divisor)==0) {
+			num = num/divisor;
+		} else {
+			divisor++;
+		}
+	}
+
+	return divisor;
+
+}
+int main(){
+
+	cout << "The largest prime factor of 600851475143 is " << largestPrime(600851475143) << "." << endl;
+
+}
+
